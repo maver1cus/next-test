f2ae1c7d40a5e6e51d33ffc92600b5fdefec0a0b
Sergey Petrov <mavericusdev@gmail.com>
2022-07-10 20:08:07 +0300

    init: initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const api = process.env.NESTJS_HOST || "http://localhost:5000";
+
 const nextConfig = {
+  rewrites: () => [{ source: "/api/:path*", destination: `${api}/:path*` }],
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
